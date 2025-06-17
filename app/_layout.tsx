5d6a8f73296fb086c00f61ad7cd2119375a0b5e5
JerielFalla <jeriel.falla@gmail.com>
2025-06-18 04:49:50 +0800

    add deeplinking

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,19 @@
-import { Slot, Stack } from 'expo-router';
+import { ExpoRoot, Slot, Stack } from 'expo-router';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import ChatProvider from '../context/ChatProvider';
 import { Provider as PaperProvider, DefaultTheme } from 'react-native-paper';
 import { useEffect, useState } from 'react';
+import { registerRootComponent } from "expo";
 
 
 export default function RootLayout() {
-
+    const ctx = require.context("./app");
 
     return (
         <GestureHandlerRootView style={{ flex: 1 }}>
             <SafeAreaProvider>
+
                 <ChatProvider>
                     <PaperProvider theme={DefaultTheme}>
                         <Stack screenOptions={{ headerShown: false }}>
@@ -19,6 +21,7 @@ export default function RootLayout() {
                             <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
                             <Stack.Screen name="(drawer)" options={{ headerShown: false }} />
                             <Stack.Screen name="(onboarding)" options={{ headerShown: false }} />
+                            <ExpoRoot context={ctx} />
                             <Slot />
                         </Stack>
                     </PaperProvider>
@@ -27,3 +30,5 @@ export default function RootLayout() {
         </GestureHandlerRootView>
     );
 }
+
+registerRootComponent(RootLayout);
